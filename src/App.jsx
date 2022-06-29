cb5e007b7d7ee88daedf08876e7531b15fd0d210
Temitope Johnson <temitopejohnson4ts@gmail.com>
2022-06-29 21:18:53 +0100

    edited product image to be responsive

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,19 @@ import './App.css'
 function App() {
   return (
     <div className="App">
-      <img id='ProductImg' srcSet='assets\image-product-desktop.jpg, assets\image-product-mobile.jpg'  alt='Product Image' />
+      <picture>
+        <source srcSet='assets\image-product-desktop.jpg' media='(min-width: 419px)'></source>
+        <source srcSet='assets\image-product-mobile.jpg' ></source>
+        <img id='ProductImg' src='assets\image-product-desktop.jpg' alt='Product Image' />
+      </picture>
       <section>
         <div className='Title'>Preview</div>
         <div className='ProductName'>Gabrielle Essence Eau De Parfum</div>
         <div className='Discription'>A floral, solar and voluptuous interpretation
           composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.</div>
         <div className='Price'>
-          <span style={{color: "#3D8168", fontFamily: 'Fraunces', fontWeight: 700, fontSize: '25px'}}>$149.99</span>
-          <span style={{fontFamily: 'Fraunces', fontSize: '10px', textDecoration: "line-through", color: 'hsl(228, 12%, 48%)'}}>$169.99</span>
+          <span style={{ color: "#3D8168", fontFamily: 'Fraunces', fontWeight: 700, fontSize: '25px' }}>$149.99</span>
+          <span style={{ fontFamily: 'Fraunces', fontSize: '10px', textDecoration: "line-through", color: 'hsl(228, 12%, 48%)' }}>$169.99</span>
         </div>
         <button>
           <img src='assets\icon-cart.svg' />
